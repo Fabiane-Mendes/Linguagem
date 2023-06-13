73b7de4d8529e61ffea4f23627ec0d864cb91a84
Fabiane-Mendes <136273619+Fabiane-Mendes@users.noreply.github.com>
2023-06-13 20:20:30 -0300

    Update header_with_seachbox.dart

--- lib/screens/home/components/header_with_seachbox.dart
+++ lib/screens/home/components/header_with_seachbox.dart
@@ -15,8 +15,10 @@ class HeaderWithSearchBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
+      // Define uma margem inferior usando a constante kDefaultPadding
       // It will cover 20% of our total height
       height: size.height * 0.2,
+      // Define a altura do container como 20% da altura total da tela
       child: Stack(
         children: <Widget>[
           Container(
@@ -25,12 +27,16 @@ class HeaderWithSearchBox extends StatelessWidget {
               right: kDefaultPadding,
               bottom: 36 + kDefaultPadding,
             ),
+            // Define um preenchimento interno no container
             height: size.height * 0.2 - 27,
+            // Define a altura do container interna
             decoration: BoxDecoration(
               color: kPrimaryColor,
+              // Define a cor de fundo do container
               borderRadius: BorderRadius.only(
                 bottomLeft: Radius.circular(36),
                 bottomRight: Radius.circular(36),
+                // Define bordas arredondadas na parte inferior do container
               ),
             ),
             child: Row(
@@ -39,9 +45,11 @@ class HeaderWithSearchBox extends StatelessWidget {
                   'Hi Uishopy!',
                   style: Theme.of(context).textTheme.headline5.copyWith(
                       color: Colors.white, fontWeight: FontWeight.bold),
+                  // Estilo do texto
                 ),
                 Spacer(),
                 Image.asset("assets/images/logo.png")
+                // Exibe uma imagem com base no caminho fornecido
               ],
             ),
           ),
@@ -52,11 +60,16 @@ class HeaderWithSearchBox extends StatelessWidget {
             child: Container(
               alignment: Alignment.center,
               margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+               // Define uma margem horizontal usando a constante kDefaultPadding
               padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              // Define um preenchimento horizontal no container
               height: 54,
+              // Define a altura do container
               decoration: BoxDecoration(
                 color: Colors.white,
+                // Define a cor de fundo do container
                 borderRadius: BorderRadius.circular(20),
+                 // Define bordas arredondadas no container
                 boxShadow: [
                   BoxShadow(
                     offset: Offset(0, 10),
@@ -64,19 +77,25 @@ class HeaderWithSearchBox extends StatelessWidget {
                     color: kPrimaryColor.withOpacity(0.23),
                   ),
                 ],
+                // Adiciona uma sombra ao container
               ),
               child: Row(
                 children: <Widget>[
                   Expanded(
                     child: TextField(
                       onChanged: (value) {},
+                       // Função chamada quando o texto é alterado
                       decoration: InputDecoration(
                         hintText: "Search",
+                        // Define o texto de dica
                         hintStyle: TextStyle(
                           color: kPrimaryColor.withOpacity(0.5),
                         ),
+                        // Define o estilo do texto de dica
                         enabledBorder: InputBorder.none,
+                         // Remove a borda quando o campo não está selecionado
                         focusedBorder: InputBorder.none,
+                        // Remove a borda quando o campo está selecionado
                         // surffix isn't working properly  with SVG
                         // thats why we use row
                         // suffixIcon: SvgPicture.asset("assets/icons/search.svg"),
@@ -84,6 +103,7 @@ class HeaderWithSearchBox extends StatelessWidget {
                     ),
                   ),
                   SvgPicture.asset("assets/icons/search.svg"),
+                  // Exibe uma imagem SVG com base no caminho fornecido
                 ],
               ),
             ),
