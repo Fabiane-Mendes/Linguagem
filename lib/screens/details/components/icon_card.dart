d81d860d2f7e4209aaa7bf56d7c4e47fda705143
Fabiane-Mendes <136273619+Fabiane-Mendes@users.noreply.github.com>
2023-06-13 20:59:35 -0300

    Update icon_card.dart

--- lib/screens/details/components/icon_card.dart
+++ lib/screens/details/components/icon_card.dart
@@ -16,26 +16,28 @@ class IconCard extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Container(
       margin: EdgeInsets.symmetric(vertical: size.height * 0.03),
+      // Define uma margem vertical proporcional à altura da tela
       padding: EdgeInsets.all(kDefaultPadding / 2),
-      height: 62,
-      width: 62,
+      // Define um preenchimento interno
+      height: 62,// Define a altura do contêiner
+      width: 62, // Define a largura do contêiner
       decoration: BoxDecoration(
-        color: kBackgroundColor,
-        borderRadius: BorderRadius.circular(6),
+        color: kBackgroundColor,// Define a cor de fundo usando a constante kBackgroundColor
+        borderRadius: BorderRadius.circular(6),// Define o raio do canto usando um valor de borda circular
         boxShadow: [
           BoxShadow(
-            offset: Offset(0, 15),
-            blurRadius: 22,
-            color: kPrimaryColor.withOpacity(0.22),
+            offset: Offset(0, 15),// Define o deslocamento da sombra em relação ao contêiner
+            blurRadius: 22,// Define o raio de desfoque da sombra
+            color: kPrimaryColor.withOpacity(0.22),// Define a cor da sombra usando a constante kPrimaryColor com opacidade reduzida
           ),
           BoxShadow(
-            offset: Offset(-15, -15),
-            blurRadius: 20,
-            color: Colors.white,
+            offset: Offset(-15, -15)// Define o deslocamento da sombra em relação ao contêiner
+            blurRadius: 20,// Define o raio de desfoque da sombra
+            color: Colors.white, // Define a cor da sombra como branco
           ),
         ],
       ),
-      child: SvgPicture.asset(icon),
+      child: SvgPicture.asset(icon),// Exibe o ícone usando o caminho do arquivo SVG fornecido
     );
   }
 }
