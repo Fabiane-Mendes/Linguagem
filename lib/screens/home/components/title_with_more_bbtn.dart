f5c3c1599ab08a567b546a4c2e84f33edb658649
Fabiane-Mendes <136273619+Fabiane-Mendes@users.noreply.github.com>
2023-06-13 19:56:17 -0300

    Update title_with_more_bbtn.dart

--- lib/screens/home/components/title_with_more_bbtn.dart
+++ lib/screens/home/components/title_with_more_bbtn.dart
@@ -15,19 +15,22 @@ class TitleWithMoreBtn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      // Define um preenchimento horizontal usando a constante kDefaultPadding
       child: Row(
         children: <Widget>[
-          TitleWithCustomUnderline(text: title),
-          Spacer(),
+          TitleWithCustomUnderline(text: title),// Widget personalizado TitleWithCustomUnderline
+          Spacer(),// Widget Spacer para criar um espaço flexível
           FlatButton(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(20),
+              // Define um formato arredondado para o botão usando um RoundedRectangleBorder
             ),
-            color: kPrimaryColor,
-            onPressed: press,
+            color: kPrimaryColor,// Cor de fundo do botão
+            onPressed: press, // Função a ser executada quando o botão for pressionado
             child: Text(
               "More",
               style: TextStyle(color: Colors.white),
+              // Texto do botão com estilo definido
             ),
           ),
         ],
@@ -48,13 +51,16 @@ class TitleWithCustomUnderline extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 24,
+      // Define a altura do container
       child: Stack(
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            // Define um preenchimento à esquerda usando a constante kDefaultPadding
             child: Text(
               text,
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              // Estilo do texto com tamanho da fonte e peso da fonte definidos
             ),
           ),
           Positioned(
@@ -63,8 +69,11 @@ class TitleWithCustomUnderline extends StatelessWidget {
             right: 0,
             child: Container(
               margin: EdgeInsets.only(right: kDefaultPadding / 4),
+              // Define uma margem à direita usando a constante kDefaultPadding
               height: 7,
+              // Define a altura da linha de sublinhado
               color: kPrimaryColor.withOpacity(0.2),
+              // Define a cor da linha de sublinhado com opacidade reduzida
             ),
           )
         ],
