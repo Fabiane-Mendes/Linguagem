a80eec7d17c6ebcc713a8d1b913d7151214d5c8f
Fabiane-Mendes <136273619+Fabiane-Mendes@users.noreply.github.com>
2023-06-13 20:48:38 -0300

    Update title_and_price.dart

--- lib/screens/details/components/title_and_price.dart
+++ lib/screens/details/components/title_and_price.dart
@@ -17,24 +17,25 @@ class TitleAndPrice extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+       // Define um espaçamento horizontal usando a constante kDefaultPadding
       child: Row(
         children: <Widget>[
           RichText(
             text: TextSpan(
               children: [
                 TextSpan(
-                  text: "$title\n",
+                  text: "$title\n",// Exibe o título na primeira linha
                   style: Theme.of(context)
                       .textTheme
                       .headline4
-                      .copyWith(color: kTextColor, fontWeight: FontWeight.bold),
+                      .copyWith(color: kTextColor, fontWeight: FontWeight.bold), // Define o estilo do texto usando o tema definido no contexto, com cor e peso da fonte personalizados
                 ),
                 TextSpan(
-                  text: country,
+                  text: country,// Exibe o país na segunda linha
                   style: TextStyle(
                     fontSize: 20,
-                    color: kPrimaryColor,
-                    fontWeight: FontWeight.w300,
+                    color: kPrimaryColor,// Define a cor do texto como a cor primária definida por kPrimaryColor
+                    fontWeight: FontWeight.w300,// Define o peso da fonte como 300 (leve)
                   ),
                 ),
               ],
