a587c1365d6663b2d72828227c214ab3a74da510
Fabiane-Mendes <136273619+Fabiane-Mendes@users.noreply.github.com>
2023-06-13 19:44:55 -0300

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+//o pacote flutter/material.dart, que é uma biblioteca do Flutter que contém vários widgets e recursos para criar interfaces de usuário.
+
 
 // Colors that we use in our app
 const kPrimaryColor = Color(0xFF0C9869);
 const kTextColor = Color(0xFF3C4046);
 const kBackgroundColor = Color(0xFFF9F8FD);
+// Essas linhas definem constantes para as cores que serão usadas no aplicativo. As cores são representadas como instâncias da classe Color do Flutter. No exemplo, temos três constantes:
+
+// kPrimaryColor: uma cor verde escuro representada pelo valor hexadecimal 0xFF0C9869.
+// kTextColor: uma cor cinza escuro representada pelo valor hexadecimal 0xFF3C4046.
+// kBackgroundColor: uma cor rosa claro representada pelo valor hexadecimal 0xFFF9F8FD.
 
 const double kDefaultPadding = 20.0;
+// Essa linha define uma constante chamada kDefaultPadding que armazena um valor numérico, neste caso, 20.0. Essa constante é usada para definir um espaçamento padrão 
+
